57f8febff3474755ff36fe8d83ed7eb3612e8b47
Gabri <gabrielsummers11@icloud.com>
2026-02-20 19:32:23 +0100

    Initial Commit

---
+++ TenTen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TenTen
+//
+//  Created by Gabriel on 20/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TenTen/TenTenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenTenApp.swift
+//  TenTen
+//
+//  Created by Gabriel on 20/2/26.
+//
+
+import SwiftUI
+
+@main
+struct TenTenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
